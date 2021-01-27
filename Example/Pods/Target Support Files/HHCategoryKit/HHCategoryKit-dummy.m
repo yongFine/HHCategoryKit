0cba7a6c93b5bf8da05e1714768686637d856cac
liu <yonghong_liu@obboiot.com>
2021-01-27 13:59:02 +0800

    first commit

---
+++ Example/Pods/Target Support Files/HHCategoryKit/HHCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HHCategoryKit : NSObject
+@end
+@implementation PodsDummy_HHCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-HHCategoryKit_Tests/Pods-HHCategoryKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HHCategoryKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HHCategoryKit_Tests
+@end
